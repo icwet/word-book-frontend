e3550f00fdece4caf0d8f242b3d7713f8a86dcd2
icwet <icwet@yandex.ru>
2020-05-05 03:58:54 +0300

    add component User

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import { App } from "./App";
 import { render } from "@testing-library/react";
@@ -7,4 +8,15 @@ test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
+});
+*/
+
+// @ts-ignore
+
+function sum(a, b) {
+  return a + b;
+}
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
+import { Provider } from "react-redux";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import store from "./store";
+
+const history = createBrowserHistory();
+
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
+    </Provider>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById("root")
 );
